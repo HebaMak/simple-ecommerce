fd020be87f1a5e5ec25f3278b73bdb3f7dbdf58f
HebaSalem <puupiiiii@gmail.com>
2022-11-26 11:29:13 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
-
-import './App.css';
-import Navbar from './components/Navbar';
-import Favorites from './pages/Favorites';
-import ProductDetails from './pages/productDetails';
-import Products from './pages/Products';
-import FavouritesContext from './context/FavouritesContext';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FavouritesContext from "./context/FavouritesContext";
+import Navbar from "./components/Navbar";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/productDetails";
+import Favorites from "./pages/Favorites";
+import "./App.css";
 
 function App() {
   return (
     <FavouritesContext>
       <Router>
-        <h1 className='title'>Products</h1>
+        <h1 className="title">Products</h1>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Products/>} ></Route>
-          <Route path='/favorites' element={<Favorites/>} />
-          <Route path='/product/:id' element={<ProductDetails/>} />
+          <Route path="/" element={<Products />}></Route>
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
         </Routes>
       </Router>
     </FavouritesContext>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,27 +1,25 @@
-import { Link } from 'react-router-dom'
-import Heart from '../components/Heart'
+import { Link } from "react-router-dom";
+import Heart from "../components/Heart";
 
-const Product = ({product}) => {
-
-  const {image , id , title , rating} = product
+const Product = ({ product }) => {
+  const { image, id, title, rating } = product;
 
   return (
     <>
       {product && (
-        <div value={title} key={id} className='product'>
-          <Link to={`/product/${id}`} >
-            <img src={image} alt={title} className='product-image'/>
-            <h3 className='product-description'>{title}</h3>
+        <div className="product">
+          <Link to={`/product/${id}`}>
+            <img src={image} alt={title} className="product-image" />
+            <h3 className="product-description">{title}</h3>
           </Link>
-          <div className='rating'>
+          <div className="rating">
             <h4>Rate: {rating.rate}</h4>
-            <Heart id={id}/>
+            <Heart id={id} />
           </div>
         </div>
-        )
-      }
+      )}
     </>
-    )
-}
+  );
+};
 
-export default Product
+export default Product;
